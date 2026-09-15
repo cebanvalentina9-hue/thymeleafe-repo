b76914bab1f37f967253aa15d803a98a95419f5c
Valentina Ceban <cebanvalentina9@gmail.com>
2026-09-15 20:24:48 +0300

    Add HelloWorld form

--- src/main/java/pixel/academy/thymeleaf_app/controller/helloWorldController.java
+++ src/main/java/pixel/academy/thymeleaf_app/controller/helloWorldController.java
@@ -1,6 +1,7 @@
 package pixel.academy.thymeleaf_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,26 @@ public class helloWorldController {
     public String showForm(){
         return "helloworld-form";
     }
+    //metoda care se ocupa de procesarea datelor din formularul HTML
+    @GetMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+    @RequestMapping("/greeting")
+    public String generateGreeting(HttpServerRequest request, Model model) {
 
+        //retrieve the request param from HTML
+        StringtheName = request.getParameter("StudentName");
+
+        //transform the input to uppercase
+        theName = theName.toUpperCase();
+
+        //buid the reponse message
+        String result = "Welcome, " + theName;
+
+        //add the message to the model
+        model.addAttribute
+         return "helloworld";
+
+    }
 }
